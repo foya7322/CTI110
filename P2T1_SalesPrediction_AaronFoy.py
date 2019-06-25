14163c1d8d8fae4db00ca347844636bc563d8c2e
foya7322 <52183108+foya7322@users.noreply.github.com>
2019-06-24 20:30:24 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_AaronFoy.py
@@ -0,0 +1,16 @@
+# Programming Excerise 
+# 6/18/2019
+# CTI-110 P2T1
+# Aaron Foy
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
+
